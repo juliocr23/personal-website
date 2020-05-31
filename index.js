bfe7feaf06d185aeb4a82aff9e90701e4b2f2ab6
juliocr23 <rosariojulio40@gmail.com>
2020-05-31 13:17:48 -0400

    Add nav-link hover

--- index.js
+++ index.js
@@ -11,22 +11,3 @@ $( ".nav-item" ).click(function(e) {
  target.classList.add("active");
 
 });
-
-$('a[href^="#"]').on('click',function (e) {
-        e.preventDefault();
-
-        var target = this.hash;
-        var $target = $(target);
-        var scroll;
-
-        if($(window).scrollTop()==0){
-            scroll =  ($target.offset().top) - 160
-        }else{
-            scroll =  ($target.offset().top) - 60
-        }
-        $('html, body').stop().animate({
-            'scrollTop': scroll
-        }, 900, 'swing', function () {
-            //window.location.hash = target;
-        });
-    });
